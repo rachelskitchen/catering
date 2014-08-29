beb43f670145193d646c5b6483113596f1e252a1
Alex Pakhunov <apakhunov@revelsystems.com>
2014-08-28 17:25:21 -0700

    Bug 13349 - Errors with email containing apostrophe

--- js/functions.js
+++ js/functions.js
@@ -31,7 +31,7 @@ var array_month = new Array("January", "February", "March", "April", "May", "Jun
 var MonthByStr = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12};
 
 // http://www.w3.org/TR/html5/forms.html#valid-e-mail-address
-var EMAIL_VALIDATION_REGEXP = /^[^\s@;]+@[^\s@;]+$/; ///^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+var EMAIL_VALIDATION_REGEXP = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 var ERROR = {},
     MSG = {};
