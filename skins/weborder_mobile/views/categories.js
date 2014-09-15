77cea72cd979346c6e7622215a95b99d0434ca18
Mikhail Manokhov <manohov@gmail.com>
2014-09-15 12:03:45 -0700

    Bug 000 - WeborderMobile: local views/categories.js file was loaded early core views/categories file that led to 'Uncaught TypeError: Cannot set property CategoriesItemView of undefined' error

--- skins/weborder_mobile/views/categories.js
+++ skins/weborder_mobile/views/categories.js
@@ -1,4 +1,26 @@
-define(["backbone", 'factory', 'generator', 'list'], function(Backbone) {
+/*
+ * Revel Systems Online Ordering Application
+ *
+ *  Copyright (C) 2014 by Revel Systems
+ *
+ * This file is part of Revel Systems Online Ordering open source application.
+ *
+ * Revel Systems Online Ordering open source application is free software: you
+ * can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Revel Systems Online Ordering open source application is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Revel Systems Online Ordering Application.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+define(['backbone', 'categories_view'], function(Backbone) {
 
   App.Views.CategoriesView.CategoriesItemView = App.Views.LazyItemView.extend({
         name: 'categories',
@@ -40,7 +62,7 @@ define(["backbone", 'factory', 'generator', 'list'], function(Backbone) {
             this.listenTo(this.collection, 'load_complete', this.render, this);
         },
         render: function() {
-            App.Views.LazyListView.prototype.render.apply(this, arguments);           
+            App.Views.LazyListView.prototype.render.apply(this, arguments);
             return this;
         },
         addItem: function(model) {
@@ -55,4 +77,3 @@ define(["backbone", 'factory', 'generator', 'list'], function(Backbone) {
         }
     });
 });
-   
