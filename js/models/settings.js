ce77b956fda578c6fb391d364ba435154ddb9da4
Anatoly Dmitriev <anadmit@mera.ru>
2014-12-24 12:39:37 +0300

    Bug 17548 - 'Please setup at least one payment option' to be displayed when app is accessed directly

--- js/models/settings.js
+++ js/models/settings.js
@@ -367,20 +367,14 @@ define(["backbone", "async"], function(Backbone) {
                             App.Settings = App.Data.settings.get("settings_system");
 
                             if (!self.get_payment_process()) { // get payment processors
-
-                                if (App.Data.dirMode) { // app accessed via Directory app
-
+                                if (App.Data.dirMode) { // app accessed via Directory app (bug #17548)
                                     settings_system.online_orders = false; // if all payment processors are disabled this case looks like 'online_orders' is checked off because 'online_orders' affects only order creating functionality
-
-                                } else { // app accessed directly from browser
-
+                                } else { // app accessed directly from browser (bug #17548)
                                     self.set({
                                         'isMaintenance': true,
                                         'maintenanceMessage': ERROR[MAINTENANCE.PAYMENT_OPTION]
                                     });
-
                                 }
-
                             }
 
                             if (settings_system.online_orders && settings_system.dining_options.length == 0) {
