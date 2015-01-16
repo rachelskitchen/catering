45d1c8357e2e1e81283fa44773edc6cc5d75f686
Anatoly Dmitriev <anadmit@mera.ru>
2015-01-16 17:04:27 +0300

    Bug 18127: Index page doesn't display "Change?" link after payment is completed

--- skins/retail/router.js
+++ skins/retail/router.js
@@ -361,8 +361,8 @@ define(["backbone", "main_router"], function(Backbone) {
         * Get a stores list.
         */
         getEstablishments: function() {
-            this.callback = function() {
-                App.Data.mainModel.set('needShowStoreChoice', true);
+            this.getEstablishmentsCallback = function() {
+                if (/^(index.*)?$/i.test(Backbone.history.fragment)) App.Data.mainModel.set('needShowStoreChoice', true);
             };
             App.Routers.MainRouter.prototype.getEstablishments.apply(this, arguments);
         },
