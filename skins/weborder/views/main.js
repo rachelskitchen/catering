14772b7074b76265f6e41b151128724c5ded2bb1
Mikhail Manokhov <mmanokhov@revelsystems.com>
2015-01-28 15:29:11 +0300

    Bug 7861 - Directory app - desktop version

--- skins/weborder/views/main.js
+++ skins/weborder/views/main.js
@@ -56,7 +56,8 @@ define(["done_view", "generator"], function(done_view) {
         },
         events: {
             'click #popup .cancel': 'hide_popup',
-            'click .change_establishment': 'change_establishment'
+            'click .change_establishment': 'change_establishment',
+            'click .go-to-directory': 'goToDirectory'
         },
         content_change: function() {
             var content = this.$('#content'),
@@ -268,6 +269,10 @@ define(["done_view", "generator"], function(done_view) {
             // http://caniuse.com/#search=filter
             var mainEl = this.$('.main_el');
             this.model.get('isBlurContent') ? mainEl.addClass('blur') : mainEl.removeClass('blur');
+        },
+        goToDirectory: function() {
+            var goToDirectory = this.model.get('goToDirectory');
+            typeof goToDirectory == 'function' && goToDirectory();
         }
     });
 
@@ -279,10 +284,15 @@ define(["done_view", "generator"], function(done_view) {
             this.listenToOnce(App.Data.mainModel, 'loadCompleted', App.Data.myorder.check_maintenance);
         },
         events: {
-            "click .btn": 'reload'
+            "click .btn": 'reload',
+            'click .go-to-directory': 'goToDirectory'
         },
         reload: function() {
             window.location.replace(window.location.href.replace(/#.*$/, ''));
+        },
+        goToDirectory: function() {
+            var goToDirectory = this.model.get('goToDirectory');
+            typeof goToDirectory == 'function' && goToDirectory();
         }
     });
 
