65c611bc0a3953546b6db0d5fe95009ed4f0a68e
Anatoly Dmitriev <anadmit@mera.ru>
2015-03-05 13:19:32 +0300

    Bug 18410 - Web Orders: double customer name is displayed for the order on iPad if enter the first name with the space

--- js/models/card.js
+++ js/models/card.js
@@ -40,6 +40,18 @@ define(["backbone"], function(Backbone) {
         },
         initialize: function() {
             this.syncWithRevelAPI();
+            this.listenTo(this, 'change:firstName', function() {
+                var firstName = this.get('firstName');
+                (typeof(firstName) == 'string') ?
+                    this.set('firstName', Backbone.$.trim(firstName)) :
+                    this.set('firstName', this.defaults.firstName);
+            }, this);
+            this.listenTo(this, 'change:secondName', function() {
+                var secondName = this.get('secondName');
+                (typeof(secondName) == 'string') ?
+                    this.set('secondName', Backbone.$.trim(secondName)) :
+                    this.set('secondName', this.defaults.secondName);
+            }, this);
         },
         /**
         * Save current state model in storage (detected automatic).
