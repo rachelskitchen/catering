51bf1aa45bce1061544bf730168a9a3a032e6f0f
Anatoly Dmitriev <anadmit@mera.ru>
2015-03-06 16:14:20 +0300

    Bug 18410 - Web Orders: double customer name is displayed for the order on iPad if enter the first name with the space

--- js/views/card.js
+++ js/views/card.js
@@ -31,7 +31,9 @@ define(["backbone", "factory"], function(Backbone) {
         initialize: function() {
             App.Views.FactoryView.prototype.initialize.apply(this, arguments);
             this.listenTo(this.model, 'add_card', this.setData, this);
-            this.listenTo(this.model, 'change:firstName change:secondName', this.updateData, this); // update first name & last name of view
+            this.listenTo(this.model, 'change:firstName change:secondName', function(model, val, opts) {
+                this.updateData.apply(this, arguments);
+            }, this); // update first name & last name of view
         },
         render: function() {
             var expYear, expMonth, cardNumber, securityCode, model = {}, self = this;
@@ -117,23 +119,10 @@ define(["backbone", "factory"], function(Backbone) {
                 });
                 this.value = new_value;
 
-                // bug #18410: "Web Orders: double customer name is displayed for the order on iPad if enter the first name with the space"
-                var isGapsLeft = false;
-                if (new_value.charCodeAt(0) == 32) { // if gaps at the beginning of the line then save length of line
-                    isGapsLeft = true;
-                    var lWithoutTrim = new_value.length;
-                }
                 if ( ~this.className.indexOf('first_name') ) {
-                    self.model.set('firstName', new_value);
-                    new_value = self.model.get('firstName'); // get a new value that it was trimmed at model
+                    self.model.set({firstName: new_value}, {doNotUpdateView: true});
                 } else {
-                    self.model.set('secondName', new_value);
-                    new_value = self.model.get('secondName'); // get a new value that it was trimmed at model
-                }
-                if (isGapsLeft) {
-                    var lWithTrim = new_value.length,
-                        offset = lWithoutTrim - lWithTrim;
-                    start -= offset, end -= offset;
+                    self.model.set({secondName: new_value}, {doNotUpdateView: true});
                 }
 
                 try {
@@ -179,9 +168,12 @@ define(["backbone", "factory"], function(Backbone) {
         /**
          * Update first name & last name of view.
          */
-        updateData: function() {
-            this.$('.first_name').val(this.model.get('firstName'));
-            this.$('.last_name').val(this.model.get('secondName'));
+        updateData: function(model, val, opts) {
+            opts = (opts instanceof Object) ? opts : {};
+            if (!opts.doNotUpdateView) {
+                this.$('.first_name').val(this.model.get('firstName'));
+                this.$('.last_name').val(this.model.get('secondName'));
+            }
         }
     });
 
