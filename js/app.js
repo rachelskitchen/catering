8d0b85bae3c7d9fcf22988ff46b1f31982731ab5
Mikhail Manokhov <manohov@gmail.com>
2015-04-09 17:41:23 +0300

    Bug 21762 - Disappearing Web Orders

--- js/app.js
+++ js/app.js
@@ -172,6 +172,13 @@
             App.Data.settings = new App.Models.Settings({
                 supported_skins: app.skins.available
             });
+
+            // if `storage_data` attribute isn't the web storage need to show a message
+            // that blocks any user actions with UI except a click on 'OK' button to reload the app
+            if(App.Data.settings.get('storage_data') !== 1) {
+                App.Data.errors.alert(ERROR.WEBSTORAGES_ARE_DISABLED, true);
+            }
+
             var settings = App.Data.settings,
                 isNotFirstLaunch = false;
 
