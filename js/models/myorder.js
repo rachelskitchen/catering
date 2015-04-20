c00a2c0e8e157f7bfc6d2843ed50b37e4022b097
Mikhail Manokhov <manohov@gmail.com>
2015-04-20 14:15:07 +0300

    Bug 21151 - [Intuit] Shipping and Tracking feature - ordering app

--- js/models/myorder.js
+++ js/models/myorder.js
@@ -1261,7 +1261,7 @@ define(["backbone", 'total', 'checkout', 'products'], function(Backbone) {
             $.extend(payment_info, customerData.payment_info);
 
             if(checkout.dining_option === 'DINING_OPTION_DELIVERY') {
-                payment_info.address = customer.addresses[App.Data.customer.isDefaultShippingAddress() ? customer.addresses.length - 1 : customer.shipping_address];
+                payment_info.address = getAddress();
             }
 
             // process payment type
@@ -1284,7 +1284,9 @@ define(["backbone", 'total', 'checkout', 'products'], function(Backbone) {
 
             if(checkout.dining_option === 'DINING_OPTION_SHIPPING') {
                 order_info.shipping = customer.shipping_services[customer.shipping_selected] || undefined;
-                order_info.customer = !order_info.shipping ? undefined : _.extend({}, order.paymentInfo);
+                order_info.customer = !order_info.shipping ? undefined : _.extend({
+                    address: getAddress()
+                }, order.paymentInfo);
             }
 
             var myorder_json = JSON.stringify(order),
@@ -1397,6 +1399,10 @@ define(["backbone", 'total', 'checkout', 'products'], function(Backbone) {
             function reportPaymentError(message) {
                 myorder.paymentResponse = {status: 'error', errorMsg: message, capturePhase: capturePhase};
                 myorder.trigger('paymentResponse');
+            }
+
+            function getAddress() {
+                return customer.addresses[App.Data.customer.isDefaultShippingAddress() ? customer.addresses.length - 1 : customer.shipping_address];
             }
         },
         getOrderSeatCallName: function(phone) {
