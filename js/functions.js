2a40b64978d156ad361952268dc7b0f173fdbf35
evegorov <evegorov@mera.ru>
2015-05-13 12:28:22 +0300

    Bug 23269 - "Error occurred: No message about the error" wording to be corrected

--- js/functions.js
+++ js/functions.js
@@ -137,6 +137,7 @@ MSG.ERROR_NO_MSG_FROM_SERVER = "No message about the error";
 MSG.ERROR_GET_DISCOUNTS = "Failed request to get discounts";
 MSG.ERROR_INCORRECT_DISCOUNT_CODE = "Type correct discount code from 4 to 16 characters";
 MSG.DISCOUNT_CODE_NOT_FOUND = "The typed discount code hasn't been found. Automatic discounts can be applied only.";
+MSG.DISCOUNT_CODE_NOT_APPLICABLE = "The typed discount code is not applicable now. Automatic discounts can be applied only.";
 // page 'Establishments' (begin)
 MSG.ESTABLISHMENTS_ERROR_NOSTORE = 'No store is available for the specified brand';
 MSG.ESTABLISHMENTS_CHOOSE_BRAND_DESKTOP = 'Choose which %s you are looking for:';
