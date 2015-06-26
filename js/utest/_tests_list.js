6929fe932f1a8cd91dcc0510dd72ad75d1d22360
Anatoly Dmitriev <anadmit@mera.ru>
2015-06-26 17:45:17 +0300

    Bug 6180 - unit tests for core

--- js/utest/_tests_list.js
+++ js/utest/_tests_list.js
@@ -1,15 +1,15 @@
 var tests_list = [
     '../core/js/utest/test_Card',
     '../core/js/utest/test_Categories',
+    '../core/js/utest/test_Checkout',
     '../core/js/utest/test_Filters',
-    '../core/js/utest/test_Rewards',
     '../core/js/utest/test_Locale',
-    '../core/js/utest/test_Products'
+    '../core/js/utest/test_Products',
+    '../core/js/utest/test_Rewards'
     /*
-    '../core/js/utest/test_Settings',
-    '../core/js/utest/test_Customers',
     '../core/js/utest/test_Checkout',
     '../core/js/utest/test_ChildProducts',
+    '../core/js/utest/test_Customers',
     '../core/js/utest/test_Delivery',
     '../core/js/utest/test_Errors',
     '../core/js/utest/test_Geolocation',
@@ -18,6 +18,7 @@ var tests_list = [
     '../core/js/utest/test_Myorder',
     '../core/js/utest/test_Orders',
     '../core/js/utest/test_Products',
+    '../core/js/utest/test_Settings',
     '../core/js/utest/test_Tax',
     '../core/js/utest/test_TimeFrm',
     '../core/js/utest/test_Timetable',
