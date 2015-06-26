680fb3ed9e3c27eb6d2f8afdb08f6d46581d4d44
Anatoly Dmitriev <anadmit@mera.ru>
2015-06-26 16:24:45 +0300

    Bug 6180 - unit tests for core

--- js/utest/_tests_list.js
+++ js/utest/_tests_list.js
@@ -1,28 +1,27 @@
 var tests_list = [
-    // "../core/js/utest/test_Settings",
-    "../core/js/utest/test_Card",
-    // "../core/js/utest/test_Categories",
-    // "../core/js/utest/test_Customers",
-    "../core/js/utest/test_Filters",
-    "../core/js/utest/test_Rewards",
-    "../core/js/utest/test_Locale",
-    "../core/js/utest/test_Products"
-/*
-    "../core/js/utest/test_Checkout",
-    "../core/js/utest/test_ChildProducts",
-    "../core/js/utest/test_Delivery",
-    "../core/js/utest/test_Errors",
-    "../core/js/utest/test_Geolocation",
-    "../core/js/utest/test_Geopoint",
-    "../core/js/utest/test_Modifiers",
-    "../core/js/utest/test_Myorder",
-    "../core/js/utest/test_Orders",
-    "../core/js/utest/test_Products",
-    "../core/js/utest/test_Tax",
-    "../core/js/utest/test_TimeFrm",
-    "../core/js/utest/test_Timetable",
-    "../core/js/utest/test_Tips",
-    "../core/js/utest/test_Total"*/
+    '../core/js/utest/test_Card',
+    '../core/js/utest/test_Categories',
+    '../core/js/utest/test_Filters',
+    '../core/js/utest/test_Rewards',
+    '../core/js/utest/test_Locale',
+    '../core/js/utest/test_Products'
+    /*
+    '../core/js/utest/test_Settings',
+    '../core/js/utest/test_Customers',
+    '../core/js/utest/test_Checkout',
+    '../core/js/utest/test_ChildProducts',
+    '../core/js/utest/test_Delivery',
+    '../core/js/utest/test_Errors',
+    '../core/js/utest/test_Geolocation',
+    '../core/js/utest/test_Geopoint',
+    '../core/js/utest/test_Modifiers',
+    '../core/js/utest/test_Myorder',
+    '../core/js/utest/test_Orders',
+    '../core/js/utest/test_Products',
+    '../core/js/utest/test_Tax',
+    '../core/js/utest/test_TimeFrm',
+    '../core/js/utest/test_Timetable',
+    '../core/js/utest/test_Tips',
+    '../core/js/utest/test_Total'
+    */
 ];
-
-//tests_list = [ "../dev/js/utest/test_Myorder" ];
