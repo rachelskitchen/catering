78d73dfec6df3471d04282307b5a91c3c6383101
Alexander BEAGON <abeagon@mera.ru>
2015-10-26 17:27:17 +0300

    Bug 30048 - Weborder: Points Reward > Reward discount disappears after changing product's quantity
    Conflicts:
    	HTML5/Web_ordering_app/index.html
    	HTML5/Web_ordering_app/js/main.js
    	HTML5/Web_ordering_app/js/models/myorder.js
    	HTML5/Web_ordering_app/skins/paypal/template/mainTemplate.html
    	HTML5/Web_ordering_app/skins/paypal/views/mainView.js
    	HTML5/Web_ordering_app/skins/weborder/views/mainView.js

--- js/models/myorder.js
+++ js/models/myorder.js
@@ -1750,7 +1750,10 @@ define(["backbone", 'total', 'checkout', 'products', 'rewards', 'stanfordcard'],
             return itemsWithDiscount;
         },
         splitAllItemsWithPointValue: function() {
-            this.each(this.splitItemWithPointValue);
+            var self = this;
+            this.each(function(item, i) {
+                self.splitItemWithPointValue(item);
+            });
         },
         splitItemWithPointValue: function(item, silentFlag) {
             silentFlag = !!silentFlag;
@@ -1770,7 +1773,7 @@ define(["backbone", 'total', 'checkout', 'products', 'rewards', 'stanfordcard'],
         },
         splitItemAfterQuantityUpdate : function(item, oldQuantity, newQuantity, silentFlag) {
             silentFlag = !!silentFlag;
-            if (item.get('discount').get('name') === 'Reward' && oldQuantity == 1 && newQuantity != 1) {
+            if (item.get('discount').get('name') === 'Item Reward' && oldQuantity == 1 && newQuantity != 1) {
                 this.splitItemWithPointValue(item, silentFlag);
             }
         }
