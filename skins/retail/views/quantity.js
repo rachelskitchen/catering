bd387baefeda3d49e7efa03436ec2ad4ddaef616
Alexander BEAGON <abeagon@mera.ru>
2015-12-02 17:17:48 +0300

    Bug 34219 - Online order craches browser.

--- skins/retail/views/quantity.js
+++ skins/retail/views/quantity.js
@@ -36,20 +36,23 @@ define(["quantity_view"], function(quantity_view) {
                 product = this.model.get_product(),
                 quantity = this.model.get('quantity'),
                 stock_amount = product.get('stock_amount'),
-                selectWrapper = this.$('.combobox-wrapper');
+                selectWrapper = this.$('.combobox-wrapper'),
+                max_amount = Math.min(stock_amount, product.defaults.stock_amount);
 
             // need hide quantity widget if parent product is selected
             if(product.isParent())
                 return this.$el.hide();
 
             stock_amount > 0 && select.empty();
-            for (var i = 1; i <= stock_amount; i++) {
+            var options = [];
+            for (var i = 1; i <= max_amount; i++) {
                 if (i === quantity) {
-                    select.append('<option selected="selected" value="' + i + '">' + i + '</option>');
+                    options.push('<option selected="selected" value="' + i + '">' + i + '</option>');
                 } else {
-                    select.append('<option value="' + i + '">' + i + '</option>');
+                    options.push('<option value="' + i + '">' + i + '</option>');
                 }
             }
+            select.append(options);
 
             if (stock_amount === 1) {
                 select.addClass('disabled');
@@ -62,10 +65,9 @@ define(["quantity_view"], function(quantity_view) {
             }
 
             if (this.combobox) {
-                //console.log("combobox destroy");
                 this.combobox.destroy();
             }
-            this.combobox = select.combobox(1, stock_amount);
+            this.combobox = select.combobox(1, max_amount);
         },
         change: function(e) {
             this.model.set('quantity', e.target.value * 1);
