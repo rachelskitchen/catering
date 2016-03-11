06247dd54b52e3e9cfa0b7a8f6da503a29aa5d9b
Egorov Evgeny <eegorov@revelsystems.com>
2016-03-11 14:54:35 +0300

    Bug 39029 - Weborder: BrainTree Payments > Credit Card payment type is absent

--- js/config.js
+++ js/config.js
@@ -405,12 +405,7 @@ define({
          * @type {string}
          * @description The actual path of [page_visibility]{@link module:page_visibility} module.
          */
-        page_visibility: 'js/common/page_visibility',
-        /**
-         * @type {string}
-         * @description The actual path of [braintree]{@link module:braintree} module.
-         */
-        braintree: "js/libs/braintree"
+        page_visibility: 'js/common/page_visibility'
     },
     /**
      * A waiting time of a module loading.
