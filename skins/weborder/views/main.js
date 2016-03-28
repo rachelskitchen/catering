6fac8bdd6c208f56e219bd874d25096c8a05199b
Mikhail Manokhov <mmanokhov@revelsystems.com>
2016-03-28 21:04:55 +0300

    Bug 40475 - Desktop Web Ordering Profile Interface clean-up

--- skins/weborder/views/main.js
+++ skins/weborder/views/main.js
@@ -384,6 +384,9 @@ define(["done_view", "generator"], function(done_view) {
     });
 
     var MainProfileView = App.Views.CoreMainView.CoreMainProfileView.extend({
+        bindings: {
+            '#header .title': 'text: profile_title'
+        },
         render: function() {
             App.Views.CoreMainView.CoreMainProfileView.prototype.render.apply(this, arguments);
             SpinnerView.prototype.createSpinner.call(this);
