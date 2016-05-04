fccf63ad181f630dc365511713367cdccf20785e
Mikhail Manokhov <mmanokhov@revelsystems.com>
2016-05-04 14:57:13 +0300

    Bug 41847 - Re-skin and Re-design desktop and mobile web online ordering GUI: Restaurant/QSR/Retail

--- skins/weborder/views/rewards.js
+++ skins/weborder/views/rewards.js
@@ -23,5 +23,97 @@
  define(["rewards_view"], function(rewards_view) {
     'use strict';
 
+    var ItemRewardsView = App.Views.CoreRewardsView.CoreRewardsItemView.extend({
+        name: 'rewards',
+        mod: 'item_item',
+        bindings: {
+            '.reward__redemption-text': 'text: redemptionText(points, _lp.REWARDS_ITEM_POINTS)'
+        }
+    });
+
+    var VisitRewardsView = App.Views.CoreRewardsView.CoreRewardsItemView.extend({
+        name: 'rewards',
+        mod: 'item_visit',
+        bindings: {
+            '.reward__redemption-text': 'text: redemptionText(points, _lp.REWARDS_VISIT_POINTS)'
+        }
+    });
+
+    var PurchaseRewardsView = App.Views.CoreRewardsView.CoreRewardsItemView.extend({
+        name: 'rewards',
+        mod: 'item_purchase',
+        bindings: {
+            '.reward__redemption-text': 'text: redemptionText(points, _lp.REWARDS_PURCHASE_POINTS)'
+        }
+    });
+
+    var RewardsInfoView = App.Views.FactoryView.extend({
+        name: 'rewards',
+        mod: 'info',
+        bindings: {
+            '.rewards-number': 'text: number',
+            '.total-points': 'text: balance_points',
+            '.total-visits': 'text: balance_visits',
+            '.total-purchases': 'text: currencyFormat(balance_purchases)',
+            '.item-rewards-box': 'toggle: length($itemRewards)',
+            '.item-rewards': 'collection: $itemRewards, itemView: "itemRewards"',
+            '.visit-rewards-box': 'toggle: length($visitRewards)',
+            '.visit-rewards': 'collection: $visitRewards, itemView: "visitRewards"',
+            '.purchase-rewards-box': 'toggle: length($purchaseRewards)',
+            '.purchase-rewards': 'collection: $purchaseRewards, itemView: "purchaseRewards"',
+            '.rewards-unavailable': 'toggle: not(length(rewards))',
+            '.total-row-points': 'classes: {hide: isNull(balance_points)}',
+            '.total-row-visits': 'classes: {hide: isNull(balance_visits)}',
+            '.total-row-purchase': 'classes: {hide: isNull(balance_purchases)}',
+            '.apply-reward': 'classes: {disabled: not(length(discounts))}'
+        },
+        events: {
+            'click .apply-reward': 'apply'
+        },
+        onEnterListeners: {
+            '.apply-reward': 'apply'
+        },
+        bindingSources: {
+            itemRewards: function() {
+                return new Backbone.Collection();
+            },
+            visitRewards: function() {
+                return new Backbone.Collection();
+            },
+            purchaseRewards: function() {
+                return new Backbone.Collection();
+            }
+        },
+        bindingFilters: {
+            isNull: function(value) {
+                return value === null;
+            }
+        },
+        rewardItem: ItemRewardsView,
+        visitRewards: VisitRewardsView,
+        purchaseRewards: PurchaseRewardsView,
+        initialize: function() {
+            this.listenTo(this.model, 'onResetData', this.remove);
+            this.listenTo(this.model.get('rewards'), 'add remove', function() {
+                this.setItemRewards();
+                this.setVisitsRewards();
+                this.setPurchaseRewards();
+            });
+            App.Views.FactoryView.prototype.initialize.apply(this, arguments);
+        },
+        apply: function() {
+            this.model.trigger('onRedemptionApplied');
+        },
+        setItemRewards: function() {
+            this.bindingSources.itemRewards.reset(this.model.get('rewards').where({type: 1}));
+        },
+        setVisitsRewards: function() {
+            this.bindingSources.visitmRewards.reset(this.model.get('rewards').where({type: 2}));
+        },
+        setPurchaseRewards: function() {
+            this.bindingSources.purchaseRewards.reset(this.model.get('rewards').where({type: 0}));
+        }
+    });
+
     return new (require('factory'))(rewards_view.initViews.bind(rewards_view), function() {});
 });
