146b97bcb11a7e5bd679b7dcac32136cdbf54097
Maksim Gorbunov <maksim.gorbunov@revelsystems.com>
2016-07-04 13:43:12 +0300

    Bug 44997 - Weborder: Payments > Change Default Card/Remove GC > Changes are not applied on Checkout screen

--- skins/weborder/router.js
+++ skins/weborder/router.js
@@ -653,11 +653,21 @@ define(["main_router"], function(main_router) {
                     }
                 });
 
-                if (customer.payments) {
+                var customerPayments = customer.payments,
+                    customerGiftCards = customer.giftCards;
+
+                if (customerPayments) {
+                    customerPayments.trigger('resetModelsAttrs');
+
+                    var primaryPayment = customerPayments.getPrimaryPayment();
+                    if (primaryPayment) {
+                        primaryPayment.setPrimaryAsSelected();
+                    }
+
                     customer.trigger('updateCheckoutPaymentTokens');
                 }
 
-                if (customer.giftCards) {
+                if (customerGiftCards) {
                     customer.trigger('updateCheckoutGiftCards');
                 }
                 
