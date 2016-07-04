8baed7f25e704fca95614c318e70a93455ceb92b
Maksim Gorbunov <maksim.gorbunov@revelsystems.com>
2016-07-04 12:37:16 +0300

    Bug 44997 - Weborder: Payments > Change Default Card/Remove GC > Changes are not applied on Checkout screen

--- js/app.js
+++ js/app.js
@@ -231,6 +231,7 @@
             app.get['srv'] == 'as3-test' && (app.REVEL_HOST = 'https://amazon-s3-test.revelup.com');
             app.get['srv'] == '2-16' && (app.REVEL_HOST = 'https://2-16.revelup.com');
             app.get['srv'] == '2-18' && (app.REVEL_HOST = 'https://2-18.revelup.com');
+            app.get['srv'] == '2-19' && (app.REVEL_HOST = 'https://2-19.revelup.com');
             app.get['srv'] == 'rde' && (app.REVEL_HOST = 'https://rde.revelup.com');
 
             // Add 'no-focus-css' class to [tabindex] element when user clicks on it.
