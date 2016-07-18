9fd66251f9d3c2e6a5b05e261bf91d32f30f791d
Egorov Evgeny <eegorov@revelsystems.com>
2016-07-18 12:35:53 +0300

    Bug 47266 - Online Ordering does not work with IE11 After 7/11/16 Backend Update

--- js/functions.js
+++ js/functions.js
@@ -174,6 +174,32 @@ function parse_get_params() {
 }
 
 /**
+ * Array.prototype.find polyfill function (for using by IE)
+ */
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
+/**
  * Loads styles and scripts.
  */
 function load_styles_and_scripts() {
