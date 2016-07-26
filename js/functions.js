dd607fd4feef12e7b90710a6ecd45396a378b864
Egorov Evgeny <eegorov@revelsystems.com>
2016-07-26 16:50:37 +0300

    Bug 44702 - Weborder: Apply Rewards/Discount Code > Info about order is displayed without applied discount in Your Order screen after payment

--- js/functions.js
+++ js/functions.js
@@ -2665,25 +2665,24 @@ function objectsDiff(a, b, props)
 /*
 *  Debug unresolved deferred object(s). It is used in dev=true mode only.
 */
-if (App.Data.devMode) {
-    function traceDeferredObjects() {
-        var jQueryDeferredFunc = $.Deferred;
-        $.Deferred = function() {
-            if (!_.isObject(App.stackDeferred))
-                App.stackDeferred = {};
+function traceDeferredObjects() {
+    var jQueryDeferredFunc = $.Deferred;
+    $.Deferred = function() {
+        if (!_.isObject(App.stackDeferred))
+            App.stackDeferred = {};
 
-            var key = (new Date).getTime() * 1000 + Math.floor(Math.random() * 0x100);
-            var e = new Error;
+        var key = (new Date).getTime() * 1000 + Math.floor(Math.random() * 0x100);
+        var e = new Error;
 
-            var dfd = jQueryDeferredFunc.apply(this, arguments);
-            //trace(key, " => new dfd !");
-            App.stackDeferred[key] = { dfd: dfd, stack: e.stack };
-            dfd.always(function() {
-                delete App.stackDeferred[key];
-                //trace(key, " => dfd done");
-            });
-            return dfd;
-        }
+        var dfd = jQueryDeferredFunc.apply(this, arguments);
+        //trace(key, " => new dfd !");
+        App.stackDeferred[key] = { dfd: dfd, stack: e.stack };
+        dfd.always(function() {
+            delete App.stackDeferred[key];
+            //trace(key, " => dfd done");
+        });
+        return dfd;
     }
 }
+
 
