0dae2f16a7271059626ad7905eb06ec9dd5222ce
Egorov Evgeny <eegorov@revelsystems.com>
2016-07-27 12:47:35 +0300

    Bug 46866 - Web Ordering - Rewards discount set as re price showing as percent

--- js/views/rewards.js
+++ js/views/rewards.js
@@ -63,7 +63,14 @@
                 return parseInt(points, 10) <= 1 ? text1 : text2;
             },
             discountAmount: function(amount, type) {
-                return type ? Number(amount).toFixed(0) + '%' : App.Settings.currency_symbol + round_monetary_currency(amount);
+                if (type == 0 || type == 2) {
+                    return App.Settings.currency_symbol + round_monetary_currency(amount);
+                } else if (type == 1){
+                    return Number(amount).toFixed(0) + '%';
+                } else {
+                    console.error("Unexpected discount type:", type);
+                    return "";
+                }
             }
         },
         /**
