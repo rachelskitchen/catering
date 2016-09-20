a9ed33591a2b14ac2cff63bd4bdd3be3706ee733
Mikhail Manokhov <mmanokhov@revelsystems.com>
2016-09-20 16:44:36 +0300

    Bug 47760 - Order History and Re-order : Web Implementation- Restaurant/QSR

--- skins/weborder_mobile/views/profile.js
+++ skins/weborder_mobile/views/profile.js
@@ -219,6 +219,8 @@ define(["profile_view", "giftcard_view", "./myorder"], function(profile_view) {
     });
 
     var ProfileOrderItemView = App.Views.MyOrderView.MyOrderItemView.extend({
+        name: 'profile',
+        mod: 'past_order',
         className: 'order-item'
     });
 
