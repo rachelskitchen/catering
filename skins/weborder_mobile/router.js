50cb10180e1791caeff23a7fd65c84147372ac4c
Maksim Gorbunov <maksim.gorbunov@revelsystems.com>
2016-10-18 13:45:14 +0300

    Bug 50923 - Weborder_mobile: Order History > Items from previous order is added to cart immediately after tapping on Previous Order

--- skins/weborder_mobile/router.js
+++ skins/weborder_mobile/router.js
@@ -1066,7 +1066,7 @@ define(["main_router"], function(main_router) {
                     var ordersReq = customer.ordersRequest;
 
                     ordersReq.done(function() {
-                        var order = customer.orders.get(order_id);
+                        var order = customer.orders.get(order_id),
                             itemsReq = customer.getOrderItems(order);
 
                         itemsReq.done(function() {
