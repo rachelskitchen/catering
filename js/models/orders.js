2b7232ea09d630cb133b740f297d2364b40dd0d3
Maksim Gorbunov <maksim.gorbunov@revelsystems.com>
2016-12-01 13:29:40 +0300

    Bug 53861 - Weborder: Order History > Incorrect Items qty is displayed in Past Orders Details

--- js/models/orders.js
+++ js/models/orders.js
@@ -788,16 +788,12 @@ define(["backbone"], function(Backbone) {
          */
         processOrders: function(orders) {
             if (Array.isArray(orders)) {
-                orders = orders.map(function(order) {
+                return orders.filter(function(order) {
                     if (order.dining_option === DINING_OPTION.DINING_OPTION_SHIPPING) {
                         order.items = order.items.filter(function(item) {
                             return !item.actual_data.is_shipping;
                         });
                     }
-                    return order;
-                });
-
-                return orders.filter(function(order) {
                     return order.items.every(function(item) {
                         return !item.combo_used && !item.has_upsell && !item.is_combo && (App.Data.is_stanford_mode ? !item.actual_data.is_gift : true);
                     });
