4843392c7a3c4e6ab009015cbaf4282ea174450e
Maksim Gorbunov <maksim.gorbunov@revelsystems.com>
2016-12-20 12:27:47 +0300

    Bug 55275 - Weborder_mobile: Order History > 'Edit' button is displayed for items in some Past Orders

--- skins/weborder_mobile/router.js
+++ skins/weborder_mobile/router.js
@@ -1008,8 +1008,22 @@ define(["main_router"], function(main_router) {
                         isReorder: Boolean(order_id),
                         total: orderCollection.total
                     },
-                    contentClass: '',
-                    content: [
+                    contentClass: ''
+                });
+
+                if (orderReq) {
+                    orderReq.always(function() {
+                        setContent();
+                        self.change_page.call(self);
+                    });
+                }
+                else {
+                    setContent();
+                    this.change_page();
+                }
+
+                function setContent() {
+                    App.Data.mainModel.set('content', [
                         {
                             modelName: 'MyOrder',
                             mod: 'Details',
@@ -1028,14 +1042,7 @@ define(["main_router"], function(main_router) {
                             className: 'myorderNote',
                             model: orderCollection.checkout
                         }
-                    ]
-                });
-
-                if (orderReq) {
-                    orderReq.always(this.change_page.bind(this));
-                }
-                else {
-                    this.change_page();
+                    ]);
                 }
 
                 function setAction(cb) {
@@ -1074,7 +1081,6 @@ define(["main_router"], function(main_router) {
                 var dfd = Backbone.$.Deferred(),
                     orders = customer.orders,
                     order = orders.get(order_id),
-                    errors = App.Data.errors,
                     req;
 
                 if (!order) {
