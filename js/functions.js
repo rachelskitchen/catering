c52de38d1776b66efbf84fe97f548b6699bd98d5
evegorov <evegorov@mera.ru>
2017-02-09 13:40:14 +0300

    Bug 57520 - Mercury: Can't submit an order when the previous one was unsuccessful

--- js/functions.js
+++ js/functions.js
@@ -1248,10 +1248,8 @@ function fistLetterToUpperCase(text) {
 /**
  * Trace function.
  */
-//function trace() {
-//    return console.log.apply(console, arguments);
-//}
 window.trace = console.log.bind(window.console);
+
 /**
  * Formats time array as string.
  * @param   {array} time - array in format ["hh", "mm"], e.g. ["23", "59"]
